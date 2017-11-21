0e634c6e40d8b4fcff1ad9d0dc5b17077decbc53
Maísa Horta Canguçu De Souza <522530@sga.pucminas.br>
2017-11-21 20:35:53 -0200

    primeiro commit

---
+++ src/main/java/br/mg/puc/devops/conversorNumeroExtenso/App.java
@@ -0,0 +1,13 @@
+package br.mg.puc.devops.conversorNumeroExtenso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
